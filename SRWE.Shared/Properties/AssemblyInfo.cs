da9c571637a22b08663c6ba72b859e39ecd262f8
Pat Hartl <pat@pathar.tl>
2023-12-28 18:43:54 -0600

    Fix naming

--- SRWE.Shared/Properties/AssemblyInfo.cs
+++ SRWE.Shared/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SWRE.Shared")]
+[assembly: AssemblyTitle("SRWE.Shared")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SWRE.Shared")]
+[assembly: AssemblyProduct("SRWE.Shared")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
